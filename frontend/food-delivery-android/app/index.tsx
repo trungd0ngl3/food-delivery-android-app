aca832fb15f58ccbaa729492327f34c23ca4d408
trungdongle <ngu0ic0dedao1@gmail.com>
2025-11-10 21:32:47 +0700

    Remove unused components and files; restructure layout and database schema for food delivery app

---
+++ frontend/food-delivery-android/app/index.tsx
@@ -0,0 +1,24 @@
+import { Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <TextInput 
+        style={{
+          backgroundColor: '#af7474ff',
+          
+        }}
+      >cc</TextInput>
+
+
+    </View>
+  );
+}
